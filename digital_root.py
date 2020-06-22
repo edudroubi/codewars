5625f881d0f0db74c744d79d92dca55403a8d1e2
edudroubi <eduardodroubi@gmail.com>
2020-06-22 18:07:40 -0300

    add digital root solution.

--- digital_root.py
+++ digital_root.py
@@ -7,3 +7,13 @@
 #    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 # 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 # 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+def digital_root(n):
+    while len(str(n)) > 1:
+        sum_digit = 0
+        for digit in str(n):
+            sum_digit += int(digit)
+        n = sum_digit
+    return(n)
+
+print(digital_root(10))
